b036f16f7e6c6b53efb54da00f700614550e8ed0
Harinath <harireddyakkati.91@gmail.com>
2023-05-12 18:26:55 +0530

    added encapse

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,11 @@
 package com.driver;
 
 public class Main {
-  
+  public static void main(String[] args){
+      RWOnly first = new RWOnly();
+      first.setName("accio");
+      System.out.println(first.getName());
+  }
+
+
 }
